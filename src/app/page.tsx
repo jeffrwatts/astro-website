38869fd8817215d861e7fef5c32a0eabf038b219
jeffrwatts <jeffrwatts@gmail.com>
2025-09-01 21:32:05 -0700

    feat: update gallery to list GCS images; remove hero from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,16 +5,8 @@ export default function Home() {
     <main style={{ minHeight: "100vh", display: "grid", placeItems: "center", padding: 24 }}>
       <div style={{ maxWidth: 1200, width: "100%", textAlign: "center" }}>
         <h1 style={{ marginBottom: 16 }}>Astrophotography</h1>
-        <p style={{ marginBottom: 24 }}>Starter image below:</p>
-        <Image
-          src="/hero.jpg"
-          alt="Uploaded astrophotography image"
-          width={1600}
-          height={1067}
-          style={{ width: "100%", height: "auto", borderRadius: 8 }}
-          priority
-        />
-        <div style={{ marginTop: 16 }}>
+        <p style={{ marginBottom: 24 }}>Explore the gallery of images.</p>
+        <div style={{ marginTop: 8 }}>
           <Link href="/gallery">View Gallery</Link>
         </div>
       </div>
