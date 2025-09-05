5aa194125308e0fd25a55f0a13ad11161e543487
jeffrwatts <jeffrwatts@gmail.com>
2025-09-04 20:36:23 -0700

    Add TypeScript declarations for LightGallery plugins

---
+++ src/types/lightgallery.d.ts
@@ -0,0 +1,7 @@
+declare module 'lg-thumbnail';
+declare module 'lg-zoom';
+declare module 'lg-fullscreen';
+declare module 'lg-autoplay';
+declare module 'lg-share';
+declare module 'lg-pager';
+declare module 'lg-rotate';
