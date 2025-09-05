bf6a1289f3110131ea8a975f9829c25a9a00cb35
jeffrwatts <jeffrwatts@gmail.com>
2025-09-04 20:39:28 -0700

    Fix LightGallery TypeScript types

--- src/types/lightgallery.d.ts
+++ src/types/lightgallery.d.ts
@@ -1,3 +1,47 @@
+declare module 'lightgallery' {
+  interface LightGallery {
+    init(): void;
+    destroy(): void;
+    on(event: string, callback: () => void): void;
+  }
+  
+  interface LightGalleryOptions {
+    plugins?: any[];
+    mode?: string;
+    cssEasing?: string;
+    speed?: number;
+    height?: string;
+    width?: string;
+    addClass?: string;
+    startClass?: string;
+    backdropDuration?: number;
+    hideBarsDelay?: number;
+    useLeft?: boolean;
+    closable?: boolean;
+    swipeToClose?: boolean;
+    closeOnTap?: boolean;
+    enableDrag?: boolean;
+    enableSwipe?: boolean;
+    dynamic?: boolean;
+    dynamicEl?: any[];
+    index?: number;
+    fullScreen?: boolean;
+    zoom?: boolean;
+    actualSize?: boolean;
+    thumbnail?: boolean;
+    thumbWidth?: number;
+    thumbHeight?: string;
+    thumbContHeight?: number;
+    share?: boolean;
+    autoplay?: boolean;
+    pager?: boolean;
+    rotate?: boolean;
+  }
+  
+  function LightGallery(element: HTMLElement, options: LightGalleryOptions): LightGallery;
+  export = LightGallery;
+}
+
 declare module 'lg-thumbnail';
 declare module 'lg-zoom';
 declare module 'lg-fullscreen';
